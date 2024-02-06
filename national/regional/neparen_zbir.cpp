5078b64df7d23f31133ae5224f100b0b4a6d0fc6
Jakov Mitrovski <jakovvmitrovski@yahoo.com>
2024-02-06 22:44:41 +0100

    add neparen_zbir solution

---
+++ national/regional/neparen_zbir.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    int br=0;
+    for (int i=a; i<=b; i++)
+    {
+        if (i%2==1)
+            br+=i;
+    }
+    cout<<br;
+    return 0;
+}
