e6804f49ea51683bc747b3998af7d46501c64fa0
Jakov Mitrovski <jakovvmitrovski@yahoo.com>
2024-02-20 10:32:16 +0100

    add fudbalska liga solution

---
+++ national/regional/fudbalska_liga.cpp
@@ -0,0 +1,24 @@
+
+using namespace std;
+
+int main()
+{
+    int n, p;
+
+    cin >> n >> p;
+
+    n = 2 * n - 2;
+
+    int x = n + 1, y = n + 1, z = n + 1;
+
+    for (int i = 0; i <= n; i++)
+        for (int j = 0; j <= n; j++)
+            for (int k = 0; k <= n; k++)
+                if (3 * i + j == p and i + j + k == n && k < z)
+                {
+                    x = i;
+                    y = j;
+                    z = k;
+                }
+
+    cout << x << " " << y << " " << z;
